7a76a75d4d415d347186a3162851bc35c2887b32
thomruns <thomdavid74@gmail.com>
2017-11-06 10:50:42 -0800

    Additional configuring of sprite generating functionality Pt 2

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -7,11 +7,13 @@
 */
 
 var gulp = require('gulp'),
-svgSprite = require('gulp-svg-sprite');
+svgSprite = require('gulp-svg-sprite'),
+rename = require('gulp-rename');
 
 var config = {
     mode: {
         css: {
+            sprite: 'sprite.svg',
             render: {
                 css: {
                     template: './gulp/templates/sprite.css'
@@ -21,9 +23,26 @@ var config = {
         }
     }
 }
-
+//create the sprite file from a folder of svg images
 gulp.task('createSprite', function() {
-    return gulp.src('./assets/images/icons/**/*.svg') //folder where svg files live
+    return gulp.src('./assets/images/icons/**/*.svg') /*folder where svg files live*/
         .pipe(svgSprite(config))
-        .pipe(gulp.dest('./temp/sprite/')); //where to put the assembled svg file
+        .pipe(gulp.dest('./temp/sprite/')); /*where to put the assembled svg file*/
 });
+
+gulp.task('copySpriteGraphic', ['createSprite'], function() {
+    return gulp.src('./temp/sprite/css/**/*.svg')
+        .pipe(gulp.dest('./assets/images/sprites'));
+});
+
+//rename the resulting css file to conform to modular schema
+gulp.task('copySpriteCSS', ['createSprite'], function() { //name in [] is a dependency
+    return gulp.src('./temp/sprite/css/*.css')
+        .pipe(rename('_sprite.css'))
+        .pipe(gulp.dest('./assets/styles/modules'));
+});
+/*combine the above tasks into single command
+* note that copySpriteCSS has a dependency of createSprite that
+* runs first. Then, copySprite is run after that task is complete
+*/
+gulp.task('icons', ['createSprite', 'copySpriteGraphic', 'copySpriteCSS']); 
