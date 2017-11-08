d9bad816b7fe5d25d1f65768e8975465531a730f
thomruns <thomdavid74@gmail.com>
2017-11-08 11:40:04 -0800

    Coded mobile menu functionality

--- assets/scripts/App.js
+++ assets/scripts/App.js
@@ -1,17 +1,3 @@
-var $ = require('jquery');
+import MobileMenu from './modules/MobileMenu';
 
-import Person from './modules/Person';
-
-class Adult extends Person {
-    payTaxes() {
-        console.log(this.name + " now owes $150 in taxes.");
-    }
-}
-
-var john = new Person("John Doe", "blue", 10);
-john.greet();
-
-var jane = new Adult("Jane Smith", "purple", 48);
-jane.greet();
-jane.payTaxes();
-
+var mobileMenu = new MobileMenu();
